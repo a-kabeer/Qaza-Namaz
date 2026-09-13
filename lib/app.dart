4f5fa79ed4d85b57a3ad25e1a8e0b0dbe6dac086
Abdul Kabeer <144008371+a-kabeer@users.noreply.github.com>
2026-09-13 19:57:51 +0500

    Task 3: integrate Material 3 light dark system themes

--- lib/app.dart
+++ lib/app.dart
@@ -1,34 +1,51 @@
 import 'package:flutter/material.dart';
 
+import 'core/theme/app_theme.dart';
 import 'data/auth/firebase_auth_repository.dart';
 import 'data/repositories/firestore_qaza_repository.dart';
 import 'domain/repositories/auth_repository.dart';
 import 'domain/repositories/qaza_repository.dart';
 import 'features/auth/auth_gate.dart';
 
-class QazaNamazApp extends StatelessWidget {
-  QazaNamazApp({
-    AuthRepository? authRepository,
-    QazaRepository? qazaRepository,
-    super.key,
-  })  : authRepository = authRepository ?? FirebaseAuthRepository(),
+class QazaNamazApp extends StatefulWidget {
+  QazaNamazApp({AuthRepository? authRepository, QazaRepository? qazaRepository, super.key})
+      : authRepository = authRepository ?? FirebaseAuthRepository(),
         qazaRepository = qazaRepository ?? FirestoreQazaRepository();
 
   final AuthRepository authRepository;
   final QazaRepository qazaRepository;
 
   @override
+  State<QazaNamazApp> createState() => _QazaNamazAppState();
+}
+
+class _QazaNamazAppState extends State<QazaNamazApp> {
+  AppThemeMode _themeMode = AppThemeMode.system;
+
+  ThemeMode get _materialThemeMode {
+    switch (_themeMode) {
+      case AppThemeMode.system:
+        return ThemeMode.system;
+      case AppThemeMode.light:
+        return ThemeMode.light;
+      case AppThemeMode.dark:
+        return ThemeMode.dark;
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Qaza Namaz',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: Colors.indigo,
-      ),
+      theme: AppTheme.light(),
+      darkTheme: AppTheme.dark(),
+      themeMode: _materialThemeMode,
       home: AuthGate(
-        authRepository: authRepository,
-        qazaRepository: qazaRepository,
+        authRepository: widget.authRepository,
+        qazaRepository: widget.qazaRepository,
+        themeMode: _themeMode,
+        onThemeModeChanged: (mode) => setState(() => _themeMode = mode),
       ),
     );
   }
