ab1bd4228312b0e40b8d8e9a141b0fa859b4d459
Abdul Kabeer <144008371+a-kabeer@users.noreply.github.com>
2026-09-15 20:22:11 +0500

    refactor: finish riverpod architecture cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'app.dart';
+import 'app/app.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
