4314576210d1e9a01109e732629d7d6ba7b31f1f
Abdul Kabeer <144008371+a-kabeer@users.noreply.github.com>
2026-09-16 16:24:38 +0500

    fix(qaza): keep prayer metadata in existing presentation extension

--- lib/core/constants/prayer_types.dart
+++ lib/core/constants/prayer_types.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/material.dart';
-
 enum PrayerType {
   fajr,
   zuhr,
@@ -24,37 +22,6 @@ extension PrayerTypeX on PrayerType {
         return 'Isha';
       case PrayerType.witr:
         return 'Witr';
-    }
-  }
-
-  IconData get icon {
-    switch (this) {
-      case PrayerType.fajr:
-        return Icons.brightness_5_rounded;
-      case PrayerType.zuhr:
-        return Icons.wb_sunny_rounded;
-      case PrayerType.asr:
-        return Icons.wb_twilight_rounded;
-      case PrayerType.maghrib:
-        return Icons.nights_stay_rounded;
-      case PrayerType.isha:
-        return Icons.nightlight_round;
-      case PrayerType.witr:
-        return Icons.star_rounded;
-    }
-  }
-
-  String get rakats {
-    switch (this) {
-      case PrayerType.fajr:
-        return '2 rakats';
-      case PrayerType.zuhr:
-      case PrayerType.asr:
-      case PrayerType.isha:
-        return '4 rakats';
-      case PrayerType.maghrib:
-      case PrayerType.witr:
-        return '3 rakats';
     }
   }
 }
