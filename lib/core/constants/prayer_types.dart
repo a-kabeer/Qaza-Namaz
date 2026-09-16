5973aad2b2e12032a01bf7c5dfc94fc8a777fa58
Abdul Kabeer <144008371+a-kabeer@users.noreply.github.com>
2026-09-16 16:22:55 +0500

    fix(qaza): restore prayer display metadata used by add flow

--- lib/core/constants/prayer_types.dart
+++ lib/core/constants/prayer_types.dart
@@ -24,6 +24,38 @@ extension PrayerTypeX on PrayerType {
         return 'Witr';
     }
   }
+
+  IconData get icon {
+    switch (this) {
+      case PrayerType.fajr:
+        return Icons.brightness_5_rounded;
+      case PrayerType.zuhr:
+        return Icons.wb_sunny_rounded;
+      case PrayerType.asr:
+        return Icons.wb_twilight_rounded;
+      case PrayerType.maghrib:
+        return Icons.nights_stay_rounded;
+      case PrayerType.isha:
+        return Icons.nightlight_round;
+      case PrayerType.witr:
+        return Icons.star_rounded;
+    }
+  }
+
+  String get rakats {
+    switch (this) {
+      case PrayerType.fajr:
+        return '2 rakats';
+      case PrayerType.zuhr:
+      case PrayerType.asr:
+      case PrayerType.isha:
+        return '4 rakats';
+      case PrayerType.maghrib:
+        return '3 rakats';
+      case PrayerType.witr:
+        return '3 rakats';
+    }
+  }
 }
 
 const allPrayerTypes = PrayerType.values;
