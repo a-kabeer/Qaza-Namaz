04a925843f40b351dfa8141500ee3a19e63cc8c0
Abdul Kabeer <144008371+a-kabeer@users.noreply.github.com>
2026-09-17 12:31:06 +0500

    fix: use inheritable local store contract in tests

--- test/support/in_memory_qaza_local_store.dart
+++ test/support/in_memory_qaza_local_store.dart
@@ -4,7 +4,7 @@ import 'package:qaza_namaz/core/constants/prayer_types.dart';
 import 'package:qaza_namaz/data/local/qaza_local_store.dart';
 
 /// Ephemeral QazaLocalStore used by tests as a deterministic local-store double.
-class InMemoryQazaLocalStore implements QazaLocalStore {
+class InMemoryQazaLocalStore extends QazaLocalStore {
   final Map<String, List<QazaRecord>> _recordsByUser = {};
   final Map<String, List<PendingSyncOp>> _outboxByUser = {};
   final Map<String, DateTime> _lastSyncByUser = {};
