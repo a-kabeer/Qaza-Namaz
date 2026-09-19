2a4350662f526bc4144b742a34a5c54609c013ac
Abdul Kabeer <144008371+a-kabeer@users.noreply.github.com>
2026-09-19 19:41:28 +0500

    fix(auth): import firebase_core for runtime config validation

--- lib/data/auth/firebase_auth_repository.dart
+++ lib/data/auth/firebase_auth_repository.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:google_sign_in/google_sign_in.dart';
