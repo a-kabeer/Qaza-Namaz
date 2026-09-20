303b3a1894c1bbe479839c07d14818e1debb6b88
Abdul Kabeer <144008371+a-kabeer@users.noreply.github.com>
2026-09-20 20:03:50 +0500

    test: fix timeout classifier test import

--- test/sync_error_classifier_test.dart
+++ test/sync_error_classifier_test.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:qaza_namaz/data/sync/sync_error_classifier.dart';
