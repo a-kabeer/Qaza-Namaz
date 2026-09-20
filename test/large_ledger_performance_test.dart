bd168e8cc0a8c4b47301f543b5168af6b1a1a00e
Abdul Kabeer <144008371+a-kabeer@users.noreply.github.com>
2026-09-20 14:37:11 +0500

    fix(test): import sync outbox types for batch stub

--- test/large_ledger_performance_test.dart
+++ test/large_ledger_performance_test.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:qaza_namaz/core/constants/prayer_types.dart';
+import 'package:qaza_namaz/data/local/qaza_local_store.dart';
 import 'package:qaza_namaz/data/repositories/offline_first_qaza_repository.dart';
 import 'package:qaza_namaz/data/sync/qaza_sync_remote_data_source.dart';
 import 'package:qaza_namaz/data/sync/sync_state.dart';
