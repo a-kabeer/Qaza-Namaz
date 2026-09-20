d1114a0ca0543216fb2a24c5d71f7ed755b933f4
Abdul Kabeer <144008371+a-kabeer@users.noreply.github.com>
2026-09-20 14:26:24 +0500

    fix(test): import sync remote contract for failing repository

--- test/offline_first_repository_test.dart
+++ test/offline_first_repository_test.dart
@@ -4,6 +4,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:qaza_namaz/core/constants/prayer_types.dart';
 import 'package:qaza_namaz/data/local/qaza_local_store.dart';
 import 'package:qaza_namaz/data/repositories/offline_first_qaza_repository.dart';
+import 'package:qaza_namaz/data/sync/qaza_sync_remote_data_source.dart';
 import 'package:qaza_namaz/data/sync/sync_state.dart';
 import 'package:qaza_namaz/domain/entities/qaza_progress.dart';
 import 'package:qaza_namaz/domain/entities/qaza_record.dart';
