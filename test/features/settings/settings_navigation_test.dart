f27824693f264b71dc052e92682ac50a5451b13f
Abdul Kabeer <144008371+a-kabeer@users.noreply.github.com>
2026-09-21 10:33:13 +0500

    test: update navigation expectations

--- test/features/settings/settings_navigation_test.dart
+++ test/features/settings/settings_navigation_test.dart
@@ -44,7 +44,7 @@ void main() {
     expect(find.text('Data & Storage'), findsOneWidget);
     expect(find.text('Export data'), findsNothing);
     expect(find.text('Import data'), findsNothing);
-    expect(find.byType(SettingsNavRow), findsNWidgets(6));
+    expect(find.byType(SettingsNavRow), findsNWidgets(5));
   });
 
   testWidgets('Data & Cloud owns the combined export/import destination',
