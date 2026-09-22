0f5e809ffba134d37de2c871935bf758f2aa65e5
Abdul Kabeer <144008371+a-kabeer@users.noreply.github.com>
2026-09-22 09:28:30 +0500

    Switch prayer repository to local calculation

--- lib/features/prayer_times/domain/prayer_times_repository.dart
+++ lib/features/prayer_times/domain/prayer_times_repository.dart
@@ -1,14 +1,13 @@
 import 'prayer_times_models.dart';
 
 abstract class PrayerTimesRepository {
-  Future<PrayerDay?> getCachedPrayerTimes(PrayerTimesRequest request);
-
   Future<PrayerDay> getPrayerTimes({
     required double latitude,
     required double longitude,
     required DateTime date,
     required CalculationMethod method,
     required AsrMethod asrMethod,
+    String? timezone,
   });
 
   Future<PrayerLocation?> getSavedLocation();
