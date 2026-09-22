bf30ceb9407da181a683c3b26ffdf13629f53b4e
Abdul Kabeer <144008371+a-kabeer@users.noreply.github.com>
2026-09-22 20:54:18 +0500

    fix(prayer-times): remove unused timezone country import

--- lib/features/prayer_times/data/offline_location_data_source.dart
+++ lib/features/prayer_times/data/offline_location_data_source.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_countries/flutter_countries.dart';
-import 'package:timezone_country/timezone_country.dart';
 
 import '../domain/prayer_times_models.dart';
 import 'location/city_search_provider.dart';
