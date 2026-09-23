e9f4f30c2382d6b252a23d70a1c6009450db9319
Abdul Kabeer <144008371+a-kabeer@users.noreply.github.com>
2026-09-23 23:23:08 +0500

    fix: correct Home module imports

--- lib/features/home/widgets/home_statistics_summary.dart
+++ lib/features/home/widgets/home_statistics_summary.dart
@@ -3,12 +3,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../app/providers.dart';
 import '../../../core/constants/prayer_types.dart';
-import '../../../core/theme/app_theme.dart';
-import '../../../core/utils/date_formatters.dart';
+import '../../../core/theme/app_theme.dart';import '../../../core/utils/date_formatters.dart';
 import '../../../core/widgets/app_card.dart';
 import '../../../core/widgets/state_widgets.dart';
 import '../../../core/widgets/app_scaffold.dart';
-import '../../../core/theme/app_theme.dart';
 import '../../../l10n/prayer_type_l10n.dart';
 
 import '../../../domain/entities/qaza_progress.dart';
