d3ca5aa7d8a94653ce36b8afd892141100e17c2b
Abdul Kabeer <144008371+a-kabeer@users.noreply.github.com>
2026-09-23 23:25:36 +0500

    fix: complete extracted Home dependencies

--- lib/features/home/providers/home_providers.dart
+++ lib/features/home/providers/home_providers.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:io' show Platform;
 
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
