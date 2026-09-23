9037ba8532f5ba42e02131387a7604e74893d865
Abdul Kabeer <144008371+a-kabeer@users.noreply.github.com>
2026-09-23 23:59:48 +0500

    feat: propagate completion outcome through controller

--- lib/features/qaza/completion/qaza_completion_controller.dart
+++ lib/features/qaza/completion/qaza_completion_controller.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../domain/entities/qaza_completion_result.dart';
+
 import '../../prayer_times/domain/qaza_restriction_service.dart';
 import 'qaza_completion_policy.dart';
 import 'qaza_completion_service.dart';
@@ -11,7 +13,7 @@ class QazaCompletionController extends Notifier<QazaCompletionState> {
   @override
   QazaCompletionState build() => const QazaCompletionState();
 
-  Future<void> completeRecord({
+  Future<QazaCompletionResult> completeRecord({
     required String userId,
     required String recordId,
     required DateTime completedAt,
@@ -22,11 +24,11 @@ class QazaCompletionController extends Notifier<QazaCompletionState> {
     state = state.copyWith(isWorking: true);
     try {
       _policy.ensureAllowed(restriction);
-      await ref.read(qazaCompletionServiceProvider).completeRecord(
-            userId: userId,
-            recordId: recordId,
-            completedAt: completedAt,
-          );
+      return await ref.read(qazaCompletionServiceProvider).completeRecord(
+        userId: userId,
+        recordId: recordId,
+        completedAt: completedAt,
+      );
     } finally {
       state = state.copyWith(isWorking: false);
     }
