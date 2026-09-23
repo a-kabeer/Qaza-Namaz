f3c6ed6374f797c0f73025ee26730e3031c93157
Abdul Kabeer <144008371+a-kabeer@users.noreply.github.com>
2026-09-23 23:25:46 +0500

    fix: complete extracted Home dependencies

--- lib/features/home/widgets/home_today_progress.dart
+++ lib/features/home/widgets/home_today_progress.dart
@@ -14,6 +14,8 @@ import '../../../core/widgets/app_button.dart';
 import '../../../core/widgets/app_card.dart';
 import '../../../core/widgets/state_widgets.dart';
 import '../../../l10n/app_localizations.dart';
+import '../../../l10n/prayer_type_l10n.dart';
+import '../../../core/widgets/progress_widgets.dart';
 import '../../../domain/entities/qaza_progress.dart';
 import '../../../domain/entities/qaza_record.dart';
 import '../../../domain/services/qaza_service.dart';
