90bf214a0317a656095e77fc072903b9ef2d4049
Abdul Kabeer <144008371+a-kabeer@users.noreply.github.com>
2026-09-25 13:58:15 +0500

    Expose Google account creation state

--- lib/domain/repositories/auth_repository.dart
+++ lib/domain/repositories/auth_repository.dart
@@ -1,5 +1,19 @@
 import '../entities/app_user.dart';
 
+class GoogleSignInResult {
+  const GoogleSignInResult({
+    required this.user,
+    required this.isNewUser,
+  });
+
+  final AppUser user;
+  final bool isNewUser;
+}
+
+abstract interface class DetailedAuthRepository {
+  Future<GoogleSignInResult> signInWithGoogleDetails();
+}
+
 abstract interface class AuthRepository {
   AppUser? get currentUser;
 
