c01ffccaaed75c642420492f47165efafa261caa
Abdul Kabeer <144008371+a-kabeer@users.noreply.github.com>
2026-09-25 19:15:04 +0500

    Fix Prayer Times removal helper syntax

--- lib/features/home/widgets/home_today_progress.dart
+++ lib/features/home/widgets/home_today_progress.dart
@@ -610,7 +610,8 @@ class _TartibUnavailable extends StatelessWidget {
   }
 }
 
-/// Prayer choice is governed only by Qaza/Sahib al-Tartib rules.\nclass _NextQazaPanelState extends ConsumerState<_NextQazaPanel> {
+/// Prayer choice is governed only by Qaza/Sahib al-Tartib rules.
+class _NextQazaPanelState extends ConsumerState<_NextQazaPanel> {
   PopupMenuItem<String> _buildPrayerMenuItem(
     BuildContext context,
     PrayerType prayer,
