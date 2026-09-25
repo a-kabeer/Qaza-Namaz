96b47ca13f2c2d72a3802741046c6dc181bb6f77
Abdul Kabeer <144008371+a-kabeer@users.noreply.github.com>
2026-09-25 08:23:46 +0500

    test: correct leap-year qaza plan expectations

--- test/qaza_plan_service_test.dart
+++ test/qaza_plan_service_test.dart
@@ -24,11 +24,11 @@ void main() {
     expect(plan, isNotNull);
     expect(plan!.startDate, DateTime(2012, 1, 1));
     expect(plan.endDate, DateTime(2014, 1, 1));
-    expect(plan.totalDays, 730);
+    expect(plan.totalDays, 731);
     expect(plan.includeWitr, isTrue);
-    expect(plan.totalPrayers, 3650);
-    expect(plan.witrCount, 730);
-    expect(plan.totalWithWitr, 4380);
+    expect(plan.totalPrayers, 3655);
+    expect(plan.witrCount, 731);
+    expect(plan.totalWithWitr, 4386);
   });
 
   test('fixed Madhab Witr rules flow into the Qaza plan', () {
