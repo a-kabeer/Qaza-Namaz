bbcf1c9f1feb580c50bdb14946c756ca8f7691db
Abdul Kabeer <144008371+a-kabeer@users.noreply.github.com>
2026-09-26 13:29:27 +0500

    chore: fix analyzer issue in home_today_progress.dart

--- lib/features/home/widgets/home_today_progress.dart
+++ lib/features/home/widgets/home_today_progress.dart
@@ -705,8 +705,6 @@ class _NextQazaPanelState extends ConsumerState<_NextQazaPanel> {
       ),
     );
 
-    final tartib = tartibAsync.valueOrNull;
-
     final header = Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
